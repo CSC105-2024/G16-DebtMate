89b8c416f26f43062eb9f48536e29bcaa61e2f6c
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-03-20 03:17:12 +0700

    add Tailwind 4 to proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="flex">
+        <a href="https://vite.dev" target="_blank" className="pl-7">
+          <img src={viteLogo} className="h-16 w-16" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" className="pl-40">
+          <img src={reactLogo} className="size-16" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
